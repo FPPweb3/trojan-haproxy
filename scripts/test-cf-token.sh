db7548841c7c21449acaeb8ba34bcc4f91b49cb5
FPPweb3 <154480521+FPPweb3@users.noreply.github.com>
2024-07-24 02:02:18 +0300

    Update test-cf-token.sh (It works!)

--- scripts/test-cf-token.sh
+++ scripts/test-cf-token.sh
@@ -1,23 +1,18 @@
 #!/bin/bash
 
-for cmd in curl grep jq; do
+for cmd in curl grep jq openssl; do
   if ! command -v "$cmd" &> /dev/null; then
     echo "ERROR: $cmd is not installed."
     exit 1
   fi
 done
 
-if ! command -v curl &> /dev/null; then
-  echo "ERROR: curl is not installed"
-  exit 1
-fi
-
 if [ -z "$CF_Token" ] || [ -z "$domain" ]; then
   echo "ERROR: CF_Token || domain is not set"
   exit 1
 fi
 
-test_rand_id=$(cat /dev/urandom | tr -dc 'a-f0-9' | fold -w 32 | head -n 1)
+test_rand_id=$(openssl rand -hex 16)
 
 test_suffix1=$(echo "$domain" | grep -oP '[^.]+\.[^.]+$')	# example.com
 test_suffix2=$(echo "$domain" | grep -oP '([^.]+\.[^.]+\.[^.]+)$')	# example.us.com
@@ -27,7 +22,7 @@ test_response=$(curl --silent --request GET \
   --header "Authorization: Bearer $CF_Token" \
   --header "Content-Type: application/json")
 
-test_zone_id=$(echo $response | jq -r --arg test_suffix1 "$test_suffix1" --arg test_suffix2 "$test_suffix2" '.result[] | select(.name==$test_suffix1 or .name==$test_suffix2) | .id' | head -1)
+test_zone_id=$(echo $test_response | jq -r --arg test_suffix1 "$test_suffix1" --arg test_suffix2 "$test_suffix2" '.result[] | select(.name==$test_suffix1 or .name==$test_suffix2) | .id' | head -1)
 
 
 if [ -z "$test_zone_id" ]; then
@@ -35,7 +30,7 @@ if [ -z "$test_zone_id" ]; then
   exit 1
 else
   #echo "Zone ID for $MAIN_DOMAIN is $zone_id"
-  response_add=$(curl --write-out "%{http_code}" --silent --output /dev/null --request POST \
+  response_add=$(curl --write-out "%{http_code}" --silent --request POST \
     --url https://api.cloudflare.com/client/v4/zones/$test_zone_id/dns_records \
     --header "Content-Type: application/json" \
     --header "Authorization: Bearer $CF_Token" \
@@ -46,10 +41,12 @@ else
     \"id\": \"$test_rand_id\"
   }")
 
-  if [ "$response_add" -ne 200 ]; then
+  if [ ${response_add: -3} -ne 200 ]; then
     echo "ERROR: NOT VALID CF_Tocken: Failed to add test DNS record"
     exit 1
   fi
+
+  test_rand_id=$(echo $response_add | sed 's/...$//' | jq -r '.result.id')
 
   response_delete=$(curl --write-out "%{http_code}" --silent --output /dev/null --request DELETE \
     --url https://api.cloudflare.com/client/v4/zones/$test_zone_id/dns_records/$test_rand_id \
