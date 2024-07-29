f4f793e56c1eef09840744696988b0bcbed8600a
FPPweb3 <154480521+FPPweb3@users.noreply.github.com>
2024-07-29 15:50:09 +0300

    Update generate-passwords.sh (Removed characters that could break the JSON file)

--- scripts/generate-passwords.sh
+++ scripts/generate-passwords.sh
@@ -3,6 +3,6 @@
 TROJAN_PASSWORDS="$1"
 
 for i in {1..3}; do
-  PASSWORD=$(tr -dc 'A-Za-z0-9!"#$%&'\''()*+,-./:;<=>?@[\]^_`{|}~' </dev/urandom | head -c 12; echo)
+  PASSWORD=$(tr -dc 'A-Za-z0-9!#$%&()*+,-./:;<=>?@[]^_`{|}~' </dev/urandom | head -c 12; echo)
   echo "$PASSWORD" >> "$TROJAN_PASSWORDS"
 done
